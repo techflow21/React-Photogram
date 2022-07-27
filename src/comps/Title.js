f174e63011812b1f2ee40fcd4f75691feb0cd2f9
bello <bellosoliu12@gmail.com>
2022-07-27 07:26:27 +0100

    first commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>SOB-PhotoGram</h1>
+      <h2>Upload Your Pictures</h2>
+      <p>...Your sure internet photo album</p>
+    </div>
+  );
+};
+
+export default Title;
